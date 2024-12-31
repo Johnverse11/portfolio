1cdc0bef7e009929b15732b9255613bdfac1e08d
John paul <johnshalem11@gmail.com>
2024-12-31 09:51:52 +0530

    Updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+
 // Typewriter Effect
 const typewriter = document.querySelector('.Typewriter__wrapper');
 const text = "Software Developer | AI Enthusiast";
@@ -53,3 +54,28 @@ async function renderPDF(url) {
     }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
+
+fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+
+        // Reinitialize Bootstrap's JavaScript for interactive components
+        var script = document.createElement('script');
+        script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js";
+        document.body.appendChild(script);
+    })
+    .catch(error => console.error('Error loading navbar:', error));
+
+    // Include the pdfjsLib library
+    const pdfjsLib = window['pdfjs-dist/build/pdf'];
